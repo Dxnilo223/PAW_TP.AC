6a56a92efa06dd4b58d3c437cd556e148cd0ae07
ReginaPaz <125509895+Pazregina19@users.noreply.github.com>
2026-04-24 16:43:17 +0100

    update

--- controllers/supermarketController.js
+++ controllers/supermarketController.js
@@ -4,19 +4,36 @@ var Supermarket = require("../models/supermarket")
 let supermarketController = {};
 
 //Supermarket List
-supermarketController.list = async (req, res) => {
-  const supermarkets = await Supermarket.find().populate('owner');
-  res.render('supermarkets/list', { supermarkets });
+supermarketController.list = (req, res) => {
+const search = req.query.search || "";
+
+  Supermarket.find({
+    name: { $regex: search, $options: "i" }
+  })
+    .sort({ createdAt: -1 })
+    .exec()
+    .then((Supermarkets) => {
+      res.render("supermarkets/list", {
+        title: "Supermarkets list",
+        supermarkets,
+      });
+    })
+    .catch((err) => {
+      console.log("Error: ", err);
+      res.status(500).send("Error loading Supermarkets");
+    });
 };
+
 
 //CREATE
 supermarketController.create = (req, res) => {
-  res.render('supermarkets/create')
+  res.render("supermarkets/create", {
+    title: "Create Supermarket",
+  });
 };
 
 // SAVE
-supermarketController.save = async (req, res) => {
-  try {
+supermarketController.save =  (req, res) => {
     const { name, description, location, schedule, method, cost } = req.body;
 
     const supermarket = new Supermarket({
@@ -32,14 +49,17 @@ supermarketController.save = async (req, res) => {
       ]
     });
 
-    await supermarket.save();
-
-    res.redirect('/supermarkets');
-
-  } catch (err) {
-    console.log(err);
-    res.send("Error creating supermarket");
-  }
+    supermarket
+      .save()
+      .then(() => {
+        console.log("Product created successfully");
+        res.redirect("/products");
+      })
+      .catch((err) => {
+        console.log(err);
+        res.redirect("/products/create");
+      });
+ 
 };
 
 // APPROVE (admin action)
